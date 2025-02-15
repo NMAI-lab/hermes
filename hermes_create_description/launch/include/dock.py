0a390e99cda4bc78190a7490d758c3a3a69edb13
Bardia Parmoun <bardiaparmoun@gmail.com>
2025-02-15 23:08:40 +0000

    initial setup with the irobot create 3 and lidar.

---
+++ hermes_create_description/launch/include/dock.py
@@ -0,0 +1,98 @@
+# Copyright 2021 iRobot Corporation. All Rights Reserved.
+# @author Emiliano Javier Borghi Orue (creativa_eborghi@irobot.com)
+#
+# Launch standard docking station in Gazebo.
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchContext, LaunchDescription, SomeSubstitutionsType, Substitution
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import Command, LaunchConfiguration, PathJoinSubstitution
+from launch_ros.actions import Node
+
+
+class OffsetParser(Substitution):
+    def __init__(
+            self,
+            number: SomeSubstitutionsType,
+            offset: float,
+    ) -> None:
+        self.__number = number
+        self.__offset = offset
+
+    def perform(
+            self,
+            context: LaunchContext = None,
+    ) -> str:
+        number = float(self.__number.perform(context))
+        return f'{number + self.__offset}'
+
+
+ARGUMENTS = []
+for pose_element in ['x', 'y', 'z', 'yaw']:
+    ARGUMENTS.append(DeclareLaunchArgument(f'{pose_element}', default_value='0.0',
+                     description=f'{pose_element} component of the dock pose.'))
+
+
+def generate_launch_description():
+    # Directory
+    pkg_create3_description = get_package_share_directory('hermes_create_description')
+    # Path
+    dock_xacro_file = PathJoinSubstitution(
+        [pkg_create3_description, 'urdf', 'dock', 'standard.urdf.xacro'])
+
+    # Launch Configurations
+    x, y, z = LaunchConfiguration('x'), LaunchConfiguration('y'), LaunchConfiguration('z')
+    yaw = LaunchConfiguration('yaw')
+    # Apply offsets
+    x_offset = OffsetParser(x, 0.157)
+    yaw_offset = OffsetParser(yaw, 3.1416)
+
+    state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='dock_state_publisher',
+        output='screen',
+        parameters=[
+            {'use_sim_time': True},
+            {'robot_description': Command(['xacro', ' ', dock_xacro_file])},
+        ],
+        remappings=[
+            ('robot_description', 'standard_dock_description'),
+        ],
+    )
+
+    spawn_model = Node(
+        package='gazebo_ros',
+        executable='spawn_entity.py',
+        name='spawn_standard_dock',
+        arguments=['-entity',
+                   'standard_dock',
+                   '-topic',
+                   'standard_dock_description',
+                   '-x', x_offset,
+                   '-y', y,
+                   '-z', z,
+                   '-Y', yaw_offset],
+        output='screen',
+    )
+
+    tf_odom_std_dock_link_publisher = Node(
+        package='tf2_ros',
+        executable='static_transform_publisher',
+        name='tf_odom_std_dock_link_publisher',
+        arguments=[x_offset, y, z,
+                   # According to documentation (http://wiki.ros.org/tf2_ros):
+                   # the order is yaw, pitch, roll
+                   yaw_offset, '0', '0',
+                   'odom', 'std_dock_link'],
+        output='screen',
+    )
+
+    # Define LaunchDescription variable
+    ld = LaunchDescription(ARGUMENTS)
+    # Add nodes to LaunchDescription
+    ld.add_action(state_publisher)
+    ld.add_action(spawn_model)
+    ld.add_action(tf_odom_std_dock_link_publisher)
+
+    return ld
---
+++ hermes_create_description/launch/include/rviz2.py
@@ -0,0 +1,68 @@
+# Copyright 2021 iRobot Corporation. All Rights Reserved.
+# @author Rodrigo Jose Causarano Nunez (rcausaran@irobot.com)
+#
+# Launch Create(R) 3 in RViz.
+
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.conditions import IfCondition
+from launch.substitutions import Command, LaunchConfiguration, PathJoinSubstitution
+from launch_ros.actions import Node
+
+# Rviz requires US locale to correctly display the wheels
+os.environ['LC_NUMERIC'] = 'en_US.UTF-8'
+
+ARGUMENTS = [
+    DeclareLaunchArgument('rviz', default_value='true',
+                          description='Start rviz')
+]
+
+
+def generate_launch_description():
+    pkg_create3_description = get_package_share_directory('hermes_create_description')
+    xacro_file = PathJoinSubstitution([pkg_create3_description, 'urdf', 'create3.urdf.xacro'])
+
+    # Rviz
+    rviz_config_dir = PathJoinSubstitution([pkg_create3_description, 'rviz', 'model.rviz'])
+    rviz_logo_dir = PathJoinSubstitution([pkg_create3_description, 'rviz', 'irobot_logo.jpg'])
+
+    rviz = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        arguments=[
+            '--display-config', rviz_config_dir,
+            '--splash-screen', rviz_logo_dir,
+        ],
+        condition=IfCondition(LaunchConfiguration('rviz')),
+    )
+
+    robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='robot_state_publisher',
+        output='screen',
+        parameters=[
+            {'use_sim_time': True},
+            {'robot_description': Command(['xacro', ' ', xacro_file])},
+        ],
+    )
+
+    joint_state_publisher = Node(
+        package='joint_state_publisher',
+        executable='joint_state_publisher',
+        name='joint_state_publisher',
+        output='screen',
+    )
+
+    # Define LaunchDescription variable
+    ld = LaunchDescription(ARGUMENTS)
+    # Add nodes to LaunchDescription
+    ld.add_action(joint_state_publisher)
+    ld.add_action(robot_state_publisher)
+    ld.add_action(rviz)
+
+    return ld
