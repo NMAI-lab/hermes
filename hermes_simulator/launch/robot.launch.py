c169516a416273307136f5e1abd5d614a8515a74
Bardia Parmoun <bardiaparmoun@gmail.com>
2025-05-06 03:10:04 +0000

    Added the general structure of the simulator package.

---
+++ hermes_simulator/launch/robot.launch.py
@@ -0,0 +1,56 @@
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import OpaqueFunction, DeclareLaunchArgument, ExecuteProcess, IncludeLaunchDescription
+from launch.conditions import IfCondition
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch_ros.actions import Node
+
+def launch_setup(context, *args, **kwargs):
+    pkg_hermes_environment = get_package_share_directory('hermes_environment')
+
+    environment_launch_file = PathJoinSubstitution(
+        [pkg_hermes_environment, 'launch', 'environment.launch.py'])
+
+    # Includes
+    robot_environment = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([environment_launch_file]),
+        launch_arguments={'rviz': LaunchConfiguration('rviz'),
+                          'gui': LaunchConfiguration('gui'),
+                          'world': LaunchConfiguration('world'),
+                          'start': LaunchConfiguration('start'),
+                          'end': LaunchConfiguration('end'),
+                          'x': LaunchConfiguration('x'),
+                          'y': LaunchConfiguration('y'),
+                          'z': LaunchConfiguration('z'),
+                          'yaw': LaunchConfiguration('yaw')}.items(),
+    )
+    
+    # Adding all the nodes
+    nodes = [
+        robot_environment
+    ]
+
+    return nodes
+
+def generate_launch_description():
+    ARGUMENTS = [
+        DeclareLaunchArgument('rviz', default_value='true',
+                              choices=['true', 'false'], 
+                              description='Start rviz.'),
+        DeclareLaunchArgument('gui', default_value='true',
+                              choices=['true', 'false'],
+                              description='Set "false" to run gazebo headless.'),
+        DeclareLaunchArgument('world', default_value='test.json',
+                              description='The environment description'),
+        DeclareLaunchArgument('start', default_value='',
+                              description='The initial beacon for the robot'),
+        DeclareLaunchArgument('end', default_value='',
+                              description='The final beacon for the robot'),
+    ]
+
+    for pose_element in ['x', 'y', 'z', 'yaw']:
+        ARGUMENTS.append(DeclareLaunchArgument(pose_element, default_value='0.0',
+                                               description=f'{pose_element} component of the robot pose.'))
+
+    return LaunchDescription(ARGUMENTS + [OpaqueFunction(function=launch_setup)]) 
---
+++ hermes_simulator/setup.py
@@ -0,0 +1,29 @@
+import os
+from setuptools import setup
+from glob import glob
+
+package_name = 'hermes_simulator'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        # Include the launch files.
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*.launch.py'))),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='bardibloo',
+    maintainer_email='bardiaparmoun@gmail.com',
+    description='Handles the sensor interpretation and the robot control of the simulator.',
+    license='BSD',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
