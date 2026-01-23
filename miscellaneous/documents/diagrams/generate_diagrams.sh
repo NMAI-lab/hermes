6eedaad0ae334a172bb7b0724b2e5778602e52c3
Bardia Parmoun <bardiaparmoun@gmail.com>
2026-01-22 23:51:17 -0500

    Added the diagrams for the documentation.

---
+++ miscellaneous/documents/diagrams/generate_diagrams.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+
+cd "$parent_path"
+
+input_directory="."
+
+plantuml="../../libs/plantuml.jar"
+
+if [ $(which dot 2>/dev/null) ]; then
+  echo "Found GraphViz"
+else
+  echo "Did not find GraphViz"
+  sudo apt install graphviz
+fi
+
+for file in "$input_directory"/*.plantuml; do
+    if [ -f "$file" ]; then
+      java -jar "$plantuml" "$file" -o "../figures/"
+      if [ $? != 0 ]; then
+          exit 1
+      fi
+      echo "Generated a diagram."
+    fi
+done
+
+echo "Successfully Generated the Diagrams"
